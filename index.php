a8e6af91b44fdf75faee83c17946082d8865d462
pakforlay <pakforlay1337@gmail.com>
2020-11-14 14:28:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     </head>
     <body>
-        <div class="mx-auto" style="width: 200px;">
+        <div class="mx-auto" style="width: 500px;">
             <h1 class="main">Yaelahdo Priv8 API</h1>
          </div>
     </body>
