e594df2bef0188a39a74fcda5411641ee02eccba
pakforlay <pakforlay1337@gmail.com>
2020-11-14 14:10:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-echo ("hello world");
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yaelahdo Priv8 API</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
