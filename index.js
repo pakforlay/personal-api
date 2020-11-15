25f3351d30aff69eb2eeb8d29c969514db013fe4
pakforlay <pakforlay1337@gmail.com>
2020-11-16 07:16:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ app.listen(port, () => {
 app.get('', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 });
+
+app.get('/api/tik', async (req, res) => {
+    res.sendFile(__dirname + '/tik.js')
+    var url = req.query.url;
+    const gets = await getVideo(url);
+    res.json(gets)
+});
+
+app.get('/api/ytVid', async (req,res) => {
+    res.sendFile(__dirname + '/ytMus.js')
+    var url = req.query.url;
+    const gets = await getYtVid(url);
+    res.json(gets)
+});
