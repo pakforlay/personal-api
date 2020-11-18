544b53411f714c67abe9dba2be6e65decf3a9dcb
pakforlay <60418550+pakforlay@users.noreply.github.com>
2020-11-18 17:56:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var eventsRouter = require('./server/routers/events-router');
 var tiktok = require('./server/routers/tik');
 var youtube = require('./server/routers/ytvid');
 var ytmus = require('./server/routers/ytmus');
+var ytmus = require('./server/routers/IG');
 
 var app = express();
 const PORT = process.env.PORT || 80;
@@ -28,6 +29,7 @@ app.use('/events', eventsRouter);
 app.use('/tiktok', tiktok);
 app.use('/ytvid', youtube);
 app.use('/ytmus', ytmus);
+app.use('/ig', IG);
 
 app.listen(PORT, () => {
     console.log(`Server Run on port ${PORT}`)
