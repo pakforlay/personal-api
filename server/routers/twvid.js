327e9cb3a53bf8d168ddca2b6e28a98e8bb65474
pakforlay <60418550+pakforlay@users.noreply.github.com>
2020-11-21 05:23:47 +0700

    Create twvid.js

---
+++ server/routers/twvid.js
@@ -0,0 +1,29 @@
+const youtube = require('express').Router();
+const puppeteer = require("puppeteer");
+
+async function getData(URL) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://downloader4twitter.com/');
+
+    await page.type('#twitter_url', `${URL}`);
+	  await page.click('body > div.header > div > div.twitter-header-content > div.twitter-body > div > form > center > div > button > svg', {delay: 300});
+
+    await page.waitForSelector('#tweeter_search_form > div.center-content > div.button-container > div:nth-child(2) > a.btn-play');
+  	await page.click('#tweeter_search_form > div.center-content > div.button-container > div:nth-child(2) > a.btn-play');
+    let getVideo = await page.$eval('#twitter_video', (element) => {
+        return element.getAttribute('src');
+    });
+    return { getVideo }
+}
+
+twvid.get('/', async (req, res) => {
+    var URL = req.query.URL;
+    const gets = await getData(URL);
+    res.json(gets)
+});
+
+module.exports = twvid;
