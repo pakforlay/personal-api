b13660f9006b9b39e40ff5c8196046958925a2f9
pakforlay <60418550+pakforlay@users.noreply.github.com>
2020-11-21 07:03:20 +0700

    Update ytmus.js

--- server/routers/ytmus.js
+++ server/routers/ytmus.js
@@ -7,18 +7,24 @@ async function ytMusic(URL) {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('https://youtubetomp3.sc/');
+    await page.goto('https://www.320youtube.com/');
 
-    await page.type('#videoURL', `${URL}`);
-	await page.waitForSelector('#ftype > optgroup:nth-child(1) > option:nth-child(5)');
-	await page.click('#conversionForm > button', {delay: 300});
-
-    await page.waitForSelector('#conversionSuccess > p:nth-child(5) > a', {delay: 10000});
-    let getMusic = await page.$eval('#conversionSuccess > p:nth-child(5) > a', (element) => {
+    await page.type('#inputUrl', `${URL}`);
+    await page.click('body > div:nth-child(1) > div:nth-child(2) > form > p:nth-child(5) > button', {delay: 300});
+    await page.waitForSelector('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-3 > img');
+    let getImages = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-3 > img', (element) => {
+	return element.getAttribute('src');
+    });
+    await page.waitForSelector('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > div.container > div > p > button');
+    await page.click('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > div.container > div > p > button');
+    await page.waitForSelector('#download > div > div > a');
+    let getVideo = await page.$eval('#download > div > div > a', (element) => {
         return element.getAttribute('href');
     });
-	browser.close()
-    return { getMusic }
+    let titleInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(1)', el => el.innerText);
+    let sizeInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(2)', el => el.innerText);
+	browser.close();
+    return { getImages, getVideo, titleInfo, sizeInfo }
 }
 
 ytmus.get('/', async (req, res) => {
