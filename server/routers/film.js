2598c9680fe526e6ba35a0ede84926135b8caa94
pakforlay <60418550+pakforlay@users.noreply.github.com>
2020-12-14 20:55:50 +0700

    Update film.js

--- server/routers/film.js
+++ server/routers/film.js
@@ -9,7 +9,7 @@ async function getFilm(URL) {
     const page = await browser.newPage();
     await page.goto('http://149.56.24.226/');
 
-    await page.type('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__query > input', `${URL}`);
+    await page.type('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div.typeahead__field > span.typeahead__query > input', `${URL}`);
 	await page.click('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__button > button', {delay: 300});
 
     await page.waitForSelector('body > main > div > section > div:nth-child(2) > div > div > div:nth-child(2) > div > div.col-xs-9.col-sm-10.search-content > h2 > a');
