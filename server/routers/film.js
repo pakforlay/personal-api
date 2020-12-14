657e7f41bbdbbefb29831331dc424e9901c7fbf0
pakforlay <60418550+pakforlay@users.noreply.github.com>
2020-12-14 21:16:25 +0700

    Update film.js

--- server/routers/film.js
+++ server/routers/film.js
@@ -7,16 +7,16 @@ async function getFilm(URL) {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('http://149.56.24.226/');
+    await page.goto('https://pahe.ph/');
 
-    await page.type('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__query > input', `${URL}`);
-	await page.click('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__button > button', {delay: 300});
+    await page.type('#s-header', `${URL}`);
+	await page.click('#searchform-header > button > i', {delay: 300});
 
-    await page.waitForSelector('body > main > div > section > div:nth-child(2) > div > div > div:nth-child(2) > div > div.col-xs-9.col-sm-10.search-content > h2 > a');
-    let getVideo = await page.$eval('body > main > div > section > div:nth-child(2) > div > div > div:nth-child(2) > div > div.col-xs-9.col-sm-10.search-content > h2 > a', (element) => {
+    await page.waitForSelector('#main-content > div.content > div.post-listing.archive-box > div > div > ul:nth-child(3) > li > div.timeline-content > h2 > a');
+    let getVideo = await page.$eval('#main-content > div.content > div.post-listing.archive-box > div > div > ul:nth-child(3) > li > div.timeline-content > h2 > a', (element) => {
         return element.getAttribute('href');
     });
-    let titleInfo = await page.$eval('body > main > div > section > div:nth-child(2) > div > div > div:nth-child(2) > div > div.col-xs-9.col-sm-10.search-content > h2', el => el.innerText);
+    let titleInfo = await page.$eval('#main-content > div.content > div.post-listing.archive-box > div > div > ul:nth-child(3) > li > div.timeline-content > h2', el => el.innerText);
 	browser.close()
     return { getFilm, titleInfo }
 }
