a15427f29bee1cc677e365a40fadf735c2d0d8d1
Aldo D. Pratama <pakforlay1337@gmail.com>
2022-06-28 15:49:15 +0700

    w
    w

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.use(express.static('client'));
 
 
 // Enable CORS on ExpressJS to avoid cross-origin errors when calling this server using AJAX
-// We are authorizing all domains to be able to manage information via AJAX (this is just for development)
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,recording-session");
